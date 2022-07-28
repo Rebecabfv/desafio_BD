5e10036f38b964e9f545808e495232d0566ccfa1
Rebecabfv <re.bfviana@gmail.com>
2022-07-28 14:32:16 -0300

    Add cache Ingrediente

--- src/main/java/com/totalshakes/wstotalshakes/domain/model/Ingrediente.java
+++ src/main/java/com/totalshakes/wstotalshakes/domain/model/Ingrediente.java
@@ -3,6 +3,10 @@ package com.totalshakes.wstotalshakes.domain.model;
 import lombok.*;
 
 import javax.persistence.*;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import java.io.Serializable;
 
 @Data
 @Builder
@@ -10,10 +14,14 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity
 @Table(name = "ingrediente")
-public class Ingrediente {
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+public class Ingrediente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+    
     private String name;
 }
