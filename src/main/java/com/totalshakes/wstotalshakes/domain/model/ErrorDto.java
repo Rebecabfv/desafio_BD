02f49988339ec6ae4be6119fb48d79ddee42dc86
Rebecabfv <re.bfviana@gmail.com>
2022-07-29 13:57:44 -0300

    Create GlobalExceptionHandler

---
+++ src/main/java/com/totalshakes/wstotalshakes/domain/model/ErrorDto.java
@@ -0,0 +1,14 @@
+package com.totalshakes.wstotalshakes.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class ErrorDto {
+    private String message;
+}
